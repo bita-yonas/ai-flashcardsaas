772525e05a05992674c66d5749aad0c6009329b8
Bitania yonas <145022434+bita-yonas@users.noreply.github.com>
2024-08-17 08:47:44 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default async function middleware(request) {
-  if (process.env.NEXT_RUNTIME !== 'edge') {
-    return clerkMiddleware()(request);
-  }
-
-  // Edge-compatible logic here (e.g., simple auth checks without Clerk)
-  return new Response('Edge runtime detected. Clerk is disabled.');
-}
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
