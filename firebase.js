1cb044d5ac22f75d19783bf2a7a4330fece05cf8
Bitania yonas <145022434+bita-yonas@users.noreply.github.com>
2024-09-19 21:24:08 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -13,6 +13,7 @@ const firebaseConfig = {
   measurementId: "G-DY4C3VQTYP"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); // Corrected function call to getFirestore
